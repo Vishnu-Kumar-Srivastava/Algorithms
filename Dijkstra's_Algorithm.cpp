83e8e99c8b7ec4fb649cd885abf9b70bbfa87c88
Vishnu Kumar Srivastava <109894961+Vishnu-Kumar-Srivastava@users.noreply.github.com>
2024-08-06 17:12:28 +0530

    Create Dijkstra's_Algorithm.cpp

---
+++ Dijkstra's_Algorithm.cpp
@@ -0,0 +1,31 @@
+
+
+int V= //no.of vertices;
+int S= //source
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+umap<int,pair<int,int>>adj;
+
+vector<int>res(V,INT_MAX);
+res[S]=0;
+
+pq.push({0,S});
+
+while(!pq.empty()){
+  int dist= pq.top().first;
+  int node= pq.top().second;
+
+  for(auto elm: adj[node]){
+    int d= elm.second;
+    ind adjNode= elm.first;
+
+    if(dist+d<res[adjNode])
+    {
+        res[adjNode]=d+dist;
+        pq.push({d+dist,adjNode});
+    }
+
+  }
+
+}
+
+
